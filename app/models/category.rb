977417a5ae50101e934723102853695635c35d22
asaqui1 <asaqui@academic.rrc.ca>
2025-11-30 20:30:24 -0600

    added validations to my category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ApplicationRecord
   has_many :products
 
+  # Validations
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10 }
+
   # Only allow these attributes for search
   def self.ransackable_attributes(auth_object = nil)
     [ "id", "name", "description", "created_at", "updated_at" ]
